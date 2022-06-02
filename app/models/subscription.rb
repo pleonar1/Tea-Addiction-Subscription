7e4a98da0bd6fcf680210d9bdc2a2a33d2b83462
Paul Leonard <paulpaulleonard@gmail.com>
2022-06-02 10:01:45 -0400

    Add enums to subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,7 @@ class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
   validates :title, :price, :status, :frequency, presence: true
+
+  enum status: [:active, :cancelled]
+  enum frequency: [:weekly, :monthly, :annually]
 end
